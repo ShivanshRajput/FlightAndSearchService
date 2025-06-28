17be57412b8e924441d40077b4df26f499e1f9ff
ShivanshRajput <shivansh.2022ug1003@iiitranchi.ac.in>
2025-06-29 03:19:08 +0530

    minor fixes

--- src/repositary/city-repo.js
+++ src/repositary/city-repo.js
@@ -27,11 +27,19 @@ class CityRepositary{
 
     async updateCity(cityId , data ) {
         try {
-            const city = await City.update(data , {
-                where: {
-                    id : cityId
-                }
-            })
+            // The below commented part also works but will not return updated object,
+            // however in PGsql then 'returning:true' parameter can be used 
+            // const city = await City.update(data , {
+            //     where: {
+            //         id : cityId
+            //     }
+            // })
+            // return city;
+
+            // for getting updated data returned in mysql we use the below approach
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("something went wrong in the repositary layer");

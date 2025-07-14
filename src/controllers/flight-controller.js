9b94679778650c5223ae63b07aed4b31949c4a62
ShivanshRajput <shivansh.2022ug1003@iiitranchi.ac.in>
2025-07-14 13:03:10 +0530

    Added flight-create request middleware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,7 +4,16 @@ const flightService = new FlightService;
 
 const create = async (req,res) => {
     try {
-        const flight = await flightService.createFlight(req.body);
+        const flightRequestData = {
+            flightNumber:req.body.flightNumber,
+            airplaneId : req.body.airplaneId,
+            srcAirportId : req.body.srcAirportId,
+            destAirportId : req.body.destAirportId,
+            arrivalTime : req.body.arrivalTime,
+            departureTime : req.body.departureTime,
+            price : req.body.price
+        }
+        const flight = await flightService.createFlight(flightRequestData);
         return res.status(201).json({
             data:flight,
             success:true,
---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,23 @@
+const validateCreateFlight = (req, res, next) =>   {
+    if(
+        !req.body.flightNumber ||
+        !req.body.airplaneId ||
+        !req.body.srcAirportId ||
+        !req.body.destAirportId ||
+        !req.body.arrivalTime ||
+        !req.body.departureTime ||
+        !req.body.price 
+    ){
+        return res.status(400).json({
+            data:{},
+            success: false,
+            message:'Invalid request body to create a flight',
+            err: 'Missing mandatory properties to create a flight'
+        })
+    }
+    next();
+}
+
+module.exports = {
+    validateCreateFlight,
+}
